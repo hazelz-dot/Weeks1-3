201359c9b9a20745c9878817e92c205ea06f2a06
Hazel Zhou <zhou42@shernet.sheridancollege.ca>
2026-01-19 11:10:31 -0500

    made a rotating clock

--- Assets/Script/RotateMe.cs
+++ Assets/Script/RotateMe.cs
@@ -3,8 +3,7 @@ using UnityEngine.InputSystem;
 
 public class RotateMe : MonoBehaviour
 {
-    public RotateMe rotateMe; 
-    public bool mouseIsOver = false; 
+    public float speed = -1; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,20 +13,8 @@ public class RotateMe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //get the mouse position
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-
-        //get the distance between the mouse and this transform position
-        float distance = Vector2.Distance(mousePos, transform.position);
-
-        //if the mouse was close-enough set a bool to true
-        if (distance < 1)
-        {
-            mouseIsOver = true;
-        }
-        else
-        {
-            mouseIsOver = false;
-        }
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.z += speed * Time.deltaTime;
+        transform.eulerAngles = newRotation; 
     }
 }
