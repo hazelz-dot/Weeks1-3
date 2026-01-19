901785650176360c34222e0a3235955f237a4b2a
Hazel Zhou <zhou42@shernet.sheridancollege.ca>
2026-01-19 11:37:40 -0500

    added sun and moon to the clock and added a cool background

---
+++ Assets/Script/SpriteChanger.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine.InputSystem; 
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+    public SpriteRenderer 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
